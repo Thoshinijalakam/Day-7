e70f9c5d6df59bdf525e29f7d79336951ac8e8cc
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-08 10:53:27 +0530

    Add files via upload

---
+++ Twopointer.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+public class Twopointer {
+    public static void main(String args[]){
+        Scanner sc =new Scanner (System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+        int l=0;
+        int r=n-1;
+        while(l<=r)
+        {
+            int temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++;
+            r--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
+/*8
+3 4 5 6 7 8 1 9
+[9, 1, 8, 7, 6, 5, 4, 3] */
